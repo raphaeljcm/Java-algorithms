46a294e74acd870e07a3707be976ff0d58234d33
Raphael Marques <raphaeljcmarques@gmail.com>
2021-11-08 14:25:53 -0300

    Removing the wrong file

--- ldp-lab6/ex2/Ex2.java
+++ ldp-lab6/ex2/Ex2.java
@@ -1,5 +1,6 @@
 package edu.newton.ldp.u6.lab6.ex2;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -21,47 +22,51 @@ public class Ex2 {
 		Scanner read = new Scanner(System.in);
 		FileInputStream fis1 = null;
 		FileInputStream fis2 = null;
+		boolean provaReal = true;
 		
 		try {
 			// Solicitar o usuário o nome de um arq existente
-			System.out.println("Please, type the name of the first file");
+			System.out.println("Por favor, digite o nome do primeiro arquivo:");
 			var fileName = read.nextLine();
+			var file1 = new File("./" + fileName);
 			
-			System.out.println("Now, type the name of the second file:");
+			System.out.println("Agora digite o nome do segundo arquivo:");
 			var fileName2 = read.nextLine();
+			var file2 = new File("./" + fileName2);
 			
 			read.close();
 			
+			// Testando se o tamanho dos arquivos são iguais
+			if(file1.length() != file2.length()) {
+				provaReal = false;
+			}
+			
+			// Fluxo de leitura
 			fis1 = new FileInputStream("./" + fileName);
 			fis2 = new FileInputStream("./" + fileName2);
 			
-			int counter;
-			int file1 = 0, file2 = 0;
+			int counter, counter1 = 0;
 			
-			// Lendo e salvando file 1
-			while((counter = fis1.read()) != -1) {
-				file1 += counter;
+			// Lendo e comparando bytes
+			while((counter = fis1.read()) != -1 && (counter1 = fis2.read()) != -1) {
+				if(counter != counter1) {
+					provaReal = false;
+				}
 			}
 			
-			// Lendo e salvando foto 2
-			while((counter = fis2.read()) != -1) {
-				file2 += counter;
-			}
-			
-			if(file1 == file2) {
-				System.out.println("Os arquivos são idênticos!");
+			if(provaReal) {
+				System.out.println("Os arquivos são idênticos.");
 			} else {
-				System.out.println("Os arquivos são diferentes");
+				System.out.println("Tente novamente, os arquivos são diferentes.");
 			}
 			
-			System.out.println(file1 + " - " + file2);
 		} catch(FileNotFoundException ex) {
 			System.out.println(ex.getMessage());
 		} finally {
 			if(fis1 != null && fis2 != null) {
 				fis1.close();
 				fis2.close();
-			}
+			} 
 		}
 	}
 }
